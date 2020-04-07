f01afb903ce7cb5e25ee5b984d7f661c705238ea
steelerx155 <steelerx155@gmail.com>
2020-04-07 18:45:02 -0400

    completed signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :recipes, only: [:index, :show, :create, :new]
-  resources :ingredients, only: [:index, :show, :create, :new]
-  resources :users, only: [:index, :show, :create, :new]
-  
+  resources :recipes
+  resources :ingredients
+  resources :users
+  # get '/signup', to: 'user#new', as: 'signup'
 
-  get '/recipes/:id', to: 'recipes#show'
+  # get '/recipes/:id', to: 'recipes#show'
   # get '/recipes/new', to: 'recipes#new'
   # get '/recipes/:id/edit', to: 'recipes#edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
