ba7648fdf951f855eae8498379a703594b687f87
steelerx155 <steelerx155@gmail.com>
2020-04-07 15:40:07 -0400

    building edit and show pages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,29 +1,30 @@
 class RecipesController < ApplicationController
-
-
-    # def index 
-    #     @recipes = Recipe.all 
-    #  end
+    def index 
+        @recipes = Recipe.all 
+    end
+    
+    def show 
+        @recipe = Recipe.find_by(id: params[:id])
+    end
 
     def new 
         @recipe = Recipe.new
     end
 
-    # def create 
-    #     @recipe = Recipe.create(recipe_params)
-    # end
+    def create 
+        recipe = Recipe.create(recipe_params)
+        redirect_to edit_recipe_path
+    end
 
-
-    # def show 
-    #     @recipe = Recipe.find(parmas[:id])
-    # end
-
+    def edit
+        @recipe = Recipe.find(params[:id])
+    end 
 
 
 
     private 
-
     def recipe_params
-        params.require(:title).permit(:content)
+        params.require(:title).permit(
+            :content)
     end
 end
