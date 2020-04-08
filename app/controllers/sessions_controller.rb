5dba6e17bf7d7d029298b061fd6e1ab2163affca
steelerx155 <steelerx155@gmail.com>
2020-04-08 16:31:36 -0400

    added sessions destory method and created sessions new

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,6 @@
 class SessionsController < ApplicationController
-
-    def home 
-        
-    end
-
     def delete
         session.clear
          redirect_to root_path
     end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
   delete '/logout' => 'sessions#delete'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
