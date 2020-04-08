567bcbe86aa6698662b1b29d927fd296f169c3b1
steelerx155 <steelerx155@gmail.com>
2020-04-08 15:55:29 -0400

    added username to users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,10 @@ class SessionsController < ApplicationController
     def home 
         
     end
+
+    def delete
+        session.clear
+         redirect_to root_path
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     end
 
     def show 
-        @user = User.find_by_id(params[:id])
+        @user = User.find_by(params[:id])
     end
 
     def edit
