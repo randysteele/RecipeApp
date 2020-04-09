78c6d448e33358d18375d111a951b6cd67c4e8ba
steelerx155 <steelerx155@gmail.com>
2020-04-09 10:57:03 -0400

    added links to layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,15 @@ class SessionsController < ApplicationController
         session.clear
          redirect_to root_path
     end
+
+    def create 
+        user = User.find_by(username: params[:user][:username])
+        if user && user.authenticate(params[:user][:username])
+        session[:user_id] = :user_id
+        redirect_to user_path(user)
+        else
+            flash[:message] = "The information you entered is incorrect, please try again"
+            redirect_to login_path
+        end
+    end
 end
