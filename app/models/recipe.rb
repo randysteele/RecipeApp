e24e71c8fbbad9acb9024734eef73d189ebaf2c7
steelerx155 <steelerx155@gmail.com>
2020-04-10 15:26:48 -0400

    added validations to recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,9 @@
 class Recipe < ApplicationRecord
     has_many :ingredients
     belongs_to :user
+    has_many :comments
+    has_many users, through: comments
+
+    validates :title, presence: true 
+    
 end
