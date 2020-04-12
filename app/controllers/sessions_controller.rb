dde9adbec22a361753ff44552c55d0bdae60ba15
steelerx155 <steelerx155@gmail.com>
2020-04-11 22:39:39 -0400

    fixed new recipe for to have ingredient, quantity and measurement

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,8 +15,23 @@ class SessionsController < ApplicationController
         end
     end
 
-    def google 
-        binding.pry
-        
+    def google       
+      #  binding.pry  
+        @user = User.find_or_create_by(email: auth["info"]["email"]) do |user|
+            user.username = auth["info"]["first_name"]
+            user.password = SecureRandom.hex(10)
+        end
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+          else
+            redirect_to '/'
+          end
     end
+
+private
+    def auth 
+        request.env['omniauth.auth']
+    end
+
 end
