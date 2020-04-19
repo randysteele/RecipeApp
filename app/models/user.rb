0c3a14200648a149b0144159d86996d41995172e
steelerx155 <steelerx155@gmail.com>
2020-04-19 13:37:32 -0400

    added commented_recipes to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     has_many :recipes
     has_many :comments
     has_many :ingredients, through: :recipes
+    has_many :commented_recipes, through: :comments
     has_secure_password
     validates :email, uniqueness: true 
 
